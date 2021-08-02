8f47aef45964553f4ab6051d8e73f24deec5cb79
Giordano <smfgiordano@gmail.com>
2021-08-02 13:13:22 -0300

    First Section

--- S1/aula6/index.js
+++ S1/aula6/index.js
@@ -5,3 +5,17 @@ console.log('Em 2000', nome, 'conheceu Maria.');
 console.log(nome, 'casou-se com Maria em 2012.');
 console.log('Maria teve 1 filho com', nome,'em 2015.');
 console.log('O filho de', nome, 'se chama Eduardo');
+
+let nome2 //declaração 
+nome2 = 'Qualquer valor';
+console.log(nome2)
+
+
+//sem palavras reservadas
+//Variáveis precisam ter nomes significativos (mnemónicos)
+//Não deve começar o nome com número
+//Não podem conter " " ou "-"
+//Em caso de nomes compostos usar camelCase
+//variáveis Case-sensitive
+//Não podemos redeclarar variáveis com let
+//NÃO UTILIZAR VAR, UTILIZAR LET
